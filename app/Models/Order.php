0717fae032ab8434fca2a46bac12f96321e1fa31
yohaness <yohaness421@gmail.com>
2025-01-29 15:24:27 +0300

    Учитываем удаление покупателя

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -25,7 +25,7 @@ class Order extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function status()
